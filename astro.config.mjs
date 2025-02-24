a37a098672d1476981e0ca7fd8c9d4ad52d8ca0e
OCAiil <OCAiil@163.com>
2025-02-24 22:39:15 +0800

    Baidu Verify file

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.ocaiil.xyz',
+  site: 'https://ocaiil.xyz',
   base: '/',
   trailingSlash: 'always',
   integrations: [
